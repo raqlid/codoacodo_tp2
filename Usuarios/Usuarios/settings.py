04daeb3bfd5f79f6507ef0f626f332e6ba5f4f17
Ramón Félix Martorell <matemonchomaticom@gmail.com>
2023-12-09 18:38:05 -0300

    Se agrega favicon y autor a la página base

--- Usuarios/Usuarios/settings.py
+++ Usuarios/Usuarios/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'usuarios_tpi',
         'USER':'postgres',
-        'PASSWORD':'root',
+        'PASSWORD':'felix2021',
         'HOST':'127.0.0.1',
         'PORT':'5432'
     }
